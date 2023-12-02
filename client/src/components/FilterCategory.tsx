5f2a589b47c940b9104e9883aeeef7cb4b5abda9
Great Ogheneochuko <greatochuko123@gmail.com>
2023-12-02 23:03:31 +0100

    create filter category

---
+++ client/src/components/FilterCategory.tsx
@@ -0,0 +1,49 @@
+type FilterCategoryProps = {
+  category: { title: string; subCategories: string[] };
+};
+
+export default function FilterCategory({ category }: FilterCategoryProps) {
+  return (
+    <div className="flex flex-col">
+      <button
+        key={category.title}
+        className="flex items-center justify-between p-2 duration-200 group hover:text-black"
+      >
+        {category.title}
+        <span className="p-1 ">
+          <svg
+            height={20}
+            width={20}
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+            <g
+              id="SVGRepo_tracerCarrier"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            ></g>
+            <g id="SVGRepo_iconCarrier">
+              <path
+                className="duration-200 stroke-zinc-500 group-hover:stroke-black"
+                d="M9 6L15 12L9 18"
+                stroke="#000000"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              ></path>
+            </g>
+          </svg>
+        </span>
+      </button>
+      <ul className="pl-4">
+        {category.subCategories.map((subCategory) => (
+          <li className="p-1 cursor-pointer hover:text-zinc-800">
+            {subCategory}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
--- client/src/pages/SearchPage.tsx
+++ client/src/pages/SearchPage.tsx
@@ -1,7 +1,48 @@
+import FilterCategory from "../components/FilterCategory";
+
+const filterCategories = [
+  { title: "Outwear", subCategories: ["Coats", "Jackets", "Blazers", "Vests"] },
+  {
+    title: "Dresses",
+    subCategories: [
+      "Casual Dresses",
+      "Formal Dresses",
+      "Party Dresses",
+      "Summer Dresses",
+    ],
+  },
+];
+
 export default function SearchPage() {
   return (
-    <main className="pt-[70px] min-h-[80dvh] flex flex-col gap-8 mb-8">
-      SearchPage
+    <main className="pt-[80px] min-h-[80dvh] max-w-7xl mx-auto flex gap-8 mb-8 text-zinc-500">
+      <div className="w-64 border">
+        <h2 className="flex items-center justify-between p-2 text-lg font-semibold border-b">
+          Filter
+          <span>
+            <svg
+              width={20}
+              height={20}
+              viewBox="0 0 17 20"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M2.83333 6.33333L2.83333 1.75M2.83333 18.25L2.83333 10M13.8333 18.25L13.8333 10M8.33333 18.25V13.6667M8.33333 10V1.75M13.8333 6.33333L13.8333 1.75M1 6.33333H4.66667M6.5 13.6667H10.1667M12 6.33333L15.6667 6.33333"
+                stroke="#807D7E"
+                stroke-width="1.8"
+                stroke-linecap="round"
+              />
+            </svg>
+          </span>
+        </h2>
+        <div className="flex flex-col py-2 border-b ">
+          {filterCategories.map((category) => (
+            <FilterCategory category={category} />
+          ))}
+        </div>
+      </div>
+      <div className="flex-1 bg-red-500"></div>
     </main>
   );
 }
