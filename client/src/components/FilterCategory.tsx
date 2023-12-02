a47022bdc0078b3c5e54fc5da0194dece87fa873
Great Ogheneochuko <greatochuko123@gmail.com>
2023-12-02 23:41:40 +0100

    implement toggle filter category function

--- client/src/components/FilterCategory.tsx
+++ client/src/components/FilterCategory.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useSearchParams } from "react-router-dom";
 
 type FilterCategoryProps = {
   category: { title: string; subCategories: string[] };
@@ -6,16 +7,33 @@ type FilterCategoryProps = {
 
 export default function FilterCategory({ category }: FilterCategoryProps) {
   const [isOpen, setIsOpen] = useState(false);
+  const [params, setParams] = useSearchParams();
+  const filterCategory = params.get("filterCategory")?.split(",");
+  console.log(filterCategory);
 
   function toggleOpen() {
     setIsOpen((curr) => !curr);
+  }
+
+  function togglefilterCategory(category: string) {
+    if (filterCategory?.includes(category)) {
+      params.set(
+        "filterCategory",
+        filterCategory.filter((item) => item !== category).join(",")
+      );
+    } else {
+      params.set(
+        "filterCategory",
+        filterCategory ? filterCategory + "," + category : category
+      );
+    }
+    setParams(params);
   }
 
   return (
     <div className="flex flex-col">
       <button
         onClick={toggleOpen}
-        key={category.title}
         className="flex items-center justify-between p-2 duration-200 group hover:text-black"
       >
         {category.title}
@@ -27,28 +45,40 @@ export default function FilterCategory({ category }: FilterCategoryProps) {
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
           >
-            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+            <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
             <g
               id="SVGRepo_tracerCarrier"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
             ></g>
             <g id="SVGRepo_iconCarrier">
               <path
                 className="duration-200 stroke-zinc-500 group-hover:stroke-black"
                 d="M9 6L15 12L9 18"
                 stroke="#000000"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
               ></path>
             </g>
           </svg>
         </span>
       </button>
-      <ul className={`${isOpen ? "h-fit" : "h-0"} pl-4 overflow-hidden`}>
+      <ul
+        className={`${
+          isOpen ? "h-fit" : "h-0"
+        } pl-4 overflow-hidden gap-1 flex flex-col`}
+      >
         {category.subCategories.map((subCategory) => (
-          <li className="p-1 cursor-pointer hover:text-zinc-800">
+          <li
+            onClick={() => togglefilterCategory(subCategory.toLowerCase())}
+            key={subCategory}
+            className={`p-1 cursor-pointer hover:text-zinc-800 w-fit rounded-md duration-200 ${
+              filterCategory?.includes(subCategory.toLowerCase())
+                ? "bg-accent-blue-100/10"
+                : ""
+            }`}
+          >
             {subCategory}
           </li>
         ))}
