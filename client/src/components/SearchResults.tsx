5a75577fb54f1a19ff77113a34200e273d6500cb
Great Ogheneochuko <greatochuko123@gmail.com>
2023-12-03 21:20:38 +0100

    create search results component

---
+++ client/src/components/SearchResults.tsx
@@ -0,0 +1,102 @@
+import { useSearchParams } from "react-router-dom";
+import Product from "./Product";
+
+const products = [
+  {
+    name: "Relaxed Fit V-Neck Tee",
+    imgUrl: "/women-product-1.png",
+    brand: "Helen",
+    price: 299,
+  },
+  {
+    name: "Off-the-Shoulder Knit Sweater",
+    imgUrl: "/women-product-2.png",
+    brand: "Helen",
+    price: 299,
+  },
+  {
+    name: "Wrap Maxi Dress",
+    imgUrl: "/women-product-3.png",
+    brand: "Helen",
+    price: 299,
+  },
+  {
+    name: "Floral Print Ruffle Top",
+    imgUrl: "/women-product-4.png",
+    brand: "Helen",
+    price: 299,
+  },
+  {
+    name: "Classic Straight-Leg Jeans",
+    imgUrl: "/men-product-1.png",
+    brand: "Helen",
+    price: 299,
+  },
+  {
+    name: "Leather Bomber Jacket",
+    imgUrl: "/men-product-2.png",
+    brand: "Helen",
+    price: 299,
+  },
+  {
+    name: "Classic Cotton Crew Neck Tee",
+    imgUrl: "/men-product-3.png",
+    brand: "Helen",
+    price: 299,
+  },
+  {
+    name: "Plaid Flannel Shirt",
+    imgUrl: "/men-product-4.png",
+    brand: "Helen",
+    price: 299,
+  },
+];
+
+export default function SearchResults() {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const sortBy = searchParams.get("sortBy");
+  const query = searchParams.get("query") || "";
+  console.log(sortBy);
+
+  function setSortBy(sortBy: string) {
+    searchParams.set("sortBy", sortBy);
+    setSearchParams(searchParams);
+  }
+
+  return (
+    <div className="flex-1">
+      <div className="flex flex-wrap gap-2 justify-between items-center mb-4">
+        <h2>
+          Search Results for <span className="font-semibold">{query}</span>
+        </h2>
+        <div className="flex gap-4">
+          <button
+            onClick={() => setSortBy("new")}
+            className={
+              sortBy === "new" || !sortBy
+                ? "text-accent-blue-100 font-semibold"
+                : "text-zinc-400"
+            }
+          >
+            New
+          </button>
+          <button
+            onClick={() => setSortBy("recomended")}
+            className={
+              sortBy === "recomended"
+                ? "text-accent-blue-100 font-semibold"
+                : "text-zinc-400 font-medium"
+            }
+          >
+            Recomended
+          </button>
+        </div>
+      </div>
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+        {products.map((product) => (
+          <Product key={product.name} product={product} />
+        ))}
+      </div>
+    </div>
+  );
+}
--- client/src/pages/SearchPage.tsx
+++ client/src/pages/SearchPage.tsx
@@ -1,10 +1,11 @@
 import SearchFilter from "../components/SearchFilter";
+import SearchResults from "../components/SearchResults";
 
 export default function SearchPage() {
   return (
-    <main className="pt-[102px] min-h-[80dvh] max-w-7xl w-[90%] mx-auto flex flex-col md:flex-row gap-8 mb-8 text-zinc-500">
+    <main className="pt-[102px] h-fit max-w-7xl w-[90%] mx-auto flex flex-col md:flex-row gap-8 mb-8 text-zinc-500">
       <SearchFilter />
-      <div className="flex-1 bg-red-500 h-[100px] w-full">asdf</div>
+      <SearchResults />
     </main>
   );
 }
