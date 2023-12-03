053c5ba37bc5f2600d9b16a215dd44eca346db58
Great Ogheneochuko <greatochuko123@gmail.com>
2023-12-03 22:00:02 +0100

    add focus-visible state to color and make it clicable with the enter or space button

--- client/src/components/ColorFilter.tsx
+++ client/src/components/ColorFilter.tsx
@@ -35,14 +35,20 @@ export default function ColorFilter({
       <ul className="grid grid-cols-4 px-3 py-6 gap-y-4 justify-evenly">
         {colors.map((color) => (
           <li
+            tabIndex={0}
             key={color}
+            className="flex flex-col gap-2 items-center group"
             onClick={() => toggleAddColor(color)}
-            className="flex flex-col gap-2 items-center"
+            onKeyDown={(e) => {
+              if (e.code === "Enter") toggleAddColor(color);
+            }}
           >
             <div
               style={{ backgroundColor: color }}
-              className={`w-8 rounded-md aspect-square border hover:scale-105 duration-300 active:scale-90${
-                colorList.includes(color) ? " ring " : ""
+              className={`w-8 rounded-md aspect-square border hover:scale-105 duration-300 group-focus-visible:ring group-focus-visible:ring-zinc-400 active:scale-90${
+                colorList.includes(color)
+                  ? " ring group-focus-visible:ring-blue-500"
+                  : ""
               }`}
             ></div>
             <p className="text-sm capitalize">{color}</p>
