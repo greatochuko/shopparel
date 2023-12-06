5938e5580b5972ad6fe800bc31d758c735583502
Great Ogheneochuko <greatochuko123@gmail.com>
2023-12-06 11:22:09 +0100

    redesign cart grid

--- client/src/pages/CartPage.tsx
+++ client/src/pages/CartPage.tsx
@@ -36,29 +36,33 @@ const cartItems = [
 
 export default function CartPage() {
   return (
-    <main className="mt-[72px] mx-auto max-w-7xl w-[90%] pt-1">
-      <h1 className="my-4 text-zinc-400 font-semibold">
+    <main className="mt-[72px] pt-1">
+      <h1 className="my-4 text-zinc-400  mx-auto max-w-7xl w-[90%] font-semibold">
         <Link to={"/"} className="hover:underline hover:text-zinc-700">
           Home
         </Link>{" "}
         &gt; <span className="text-zinc-700">Cart</span>
       </h1>
-      <div className="flex flex-col gap-4 overflow-x-scroll">
-        <div className="flex justify-between gap-3 bg-zinc-700 uppercase py-4 last:border-none text-white">
-          <div className="flex gap-2 flex-1 min-w-[200px] uppercase text-sm font-semibold">
-            <p className="pl-4">Product Information</p>
-          </div>
-          <div className="flex-1 flex gap-2 justify-between font-semibold text-sm">
-            <div className="flex-1 min-w-[100px] flex-center">Price</div>
-            <div className="flex-1 min-w-[100px] flex-center">Quantity</div>
-            <div className="flex-1 min-w-[100px] flex-center">Shipping</div>
-            <div className="flex-1 min-w-[100px] flex-center">SubTotal</div>
-            <div className="flex-1 min-w-[100px] flex-center">Action</div>
+      <div className="overflow-x-scroll mx-auto max-w-7xl w-[90%]">
+        <div className="mx-auto min-w-fit">
+          <div className="flex justify-between gap-3 mb-4 bg-zinc-700 uppercase py-4 last:border-none text-white">
+            <div className="flex gap-2 flex-1 min-w-[200px] uppercase text-sm font-semibold">
+              <p className="pl-4">Product Information</p>
+            </div>
+            <div className="flex-1 flex gap-2 justify-between font-semibold text-sm">
+              <div className="flex-1 min-w-[100px] flex-center">Price</div>
+              <div className="flex-1 min-w-[100px] flex-center">Quantity</div>
+              <div className="flex-1 min-w-[100px] flex-center">Shipping</div>
+              <div className="flex-1 min-w-[100px] flex-center">SubTotal</div>
+              <div className="flex-1 min-w-[100px] flex-center">Action</div>
+            </div>
           </div>
         </div>
-        {cartItems.map((cartItem) => (
-          <CartItem key={cartItem._id} cartItem={cartItem} />
-        ))}
+        <div className="flex flex-col gap-4 mx-auto min-w-fit">
+          {cartItems.map((cartItem) => (
+            <CartItem key={cartItem._id} cartItem={cartItem} />
+          ))}
+        </div>
       </div>
     </main>
   );
