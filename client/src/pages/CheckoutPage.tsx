ee8f1eecca5216bfc295b20596cc64b24ec9bae4
Great Ogheneochuko <greatochuko123@gmail.com>
2023-12-08 00:16:27 +0100

    add focus styles to range inputs and shipping information

--- client/src/pages/CheckoutPage.tsx
+++ client/src/pages/CheckoutPage.tsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
-import ShippingAddress from "../components/ShippingAddress";
+import ShippingInformation from "../components/ShippingInformation";
 import PaymentMethod from "../components/PaymentMethod";
 import OrderSummary from "../components/OrderSummary";
 import { Link } from "react-router-dom";
 
-export type ShippingAddressType = {
+export type ShippingInformationType = {
   firstName: string;
   lastName: string;
   country: string;
@@ -18,26 +18,31 @@ export type ShippingAddressType = {
 };
 
 export default function CheckoutPage() {
-  const [shippingAddress, setShippingAddress] =
-    useState<ShippingAddressType | null>(null);
+  const [shippingInformation, setShippingInformation] =
+    useState<ShippingInformationType | null>(null);
 
   function handlePayment() {
     console.log("Payment Successful");
   }
 
   return (
-    <main className="mt-[78px] pt-1 flex flex-col gap-4 text-zinc-700 max-w-7xl w-[90%] mx-auto">
+    <main className="mt-[78px] pt-1 flex flex-col gap-4 text-zinc-700 max-w-7xl w-[90%] mx-auto mb-2">
       <h1 className="text-2xl font-semibold">Checkout</h1>
       <div className="relative flex gap-10">
         <div className="flex flex-col gap-10 flex-1">
-          <ShippingAddress setShippingAddress={setShippingAddress} />
-          {shippingAddress ? (
+          <ShippingInformation
+            setShippingInformation={setShippingInformation}
+          />
+          {shippingInformation ? (
             <PaymentMethod handlePayment={handlePayment} />
           ) : null}
         </div>
         <OrderSummary />
       </div>
-      <Link to={"/cart"} className="hover:underline p-1 mt-4">
+      <Link
+        to={"/cart"}
+        className="hover:underline p-1 mt-4 focus-visible:ring ring-blue-400 focus-visible:underline rounded-md w-fit"
+      >
         &larr; Back to cart
       </Link>
     </main>
