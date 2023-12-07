63f6697e8a5726fbd66fbf9bce5a599c32ca0814
Great Ogheneochuko <greatochuko123@gmail.com>
2023-12-07 16:45:10 +0100

    create payment method form

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -92,38 +92,6 @@ export default function Header() {
                     </g>
                   </svg>
                 </Link>
-                <Link
-                  title="wishlist"
-                  to={"/wishlist"}
-                  className="border border-zinc-100 focus-visible:ring-accent-blue-100 focus-visible:ring-2 grid p-1 px-1.5 group duration-200 rounded-md hover:bg-accent-blue-100 bg-zinc-100 place-content-center"
-                >
-                  <svg
-                    viewBox="0 0 24 24"
-                    height={20}
-                    width={20}
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
-                    <g
-                      id="SVGRepo_tracerCarrier"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    ></g>
-                    <g id="SVGRepo_iconCarrier">
-                      <path
-                        className="duration-200 group-hover:stroke-white"
-                        fillRule="evenodd"
-                        clipRule="evenodd"
-                        d="M12 6.00019C10.2006 3.90317 7.19377 3.2551 4.93923 5.17534C2.68468 7.09558 2.36727 10.3061 4.13778 12.5772C5.60984 14.4654 10.0648 18.4479 11.5249 19.7369C11.6882 19.8811 11.7699 19.9532 11.8652 19.9815C11.9483 20.0062 12.0393 20.0062 12.1225 19.9815C12.2178 19.9532 12.2994 19.8811 12.4628 19.7369C13.9229 18.4479 18.3778 14.4654 19.8499 12.5772C21.6204 10.3061 21.3417 7.07538 19.0484 5.17534C16.7551 3.2753 13.7994 3.90317 12 6.00019Z"
-                        stroke="#333"
-                        strokeWidth="2"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                      ></path>
-                    </g>
-                  </svg>
-                </Link>
               </>
             ) : (
               <AuthLinks />
@@ -247,40 +215,6 @@ export default function Header() {
                   </g>
                 </svg>
                 My Account
-              </Link>
-            </li>
-            <li className="sm:hidden">
-              <Link
-                to={"/wishlist"}
-                className="flex items-center gap-2 p-2 px-4 text-lg duration-300 rounded-md text-zinc-700 whitespace-nowrap hover:text-accent-blue-100 hover:bg-zinc-100 focus-visible:ring-accent-blue-100 focus-visible:ring-2"
-              >
-                <svg
-                  viewBox="0 0 24 24"
-                  height={20}
-                  width={20}
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
-                  <g
-                    id="SVGRepo_tracerCarrier"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  ></g>
-                  <g id="SVGRepo_iconCarrier">
-                    <path
-                      className="duration-200 group-hover:stroke-white"
-                      fillRule="evenodd"
-                      clipRule="evenodd"
-                      d="M12 6.00019C10.2006 3.90317 7.19377 3.2551 4.93923 5.17534C2.68468 7.09558 2.36727 10.3061 4.13778 12.5772C5.60984 14.4654 10.0648 18.4479 11.5249 19.7369C11.6882 19.8811 11.7699 19.9532 11.8652 19.9815C11.9483 20.0062 12.0393 20.0062 12.1225 19.9815C12.2178 19.9532 12.2994 19.8811 12.4628 19.7369C13.9229 18.4479 18.3778 14.4654 19.8499 12.5772C21.6204 10.3061 21.3417 7.07538 19.0484 5.17534C16.7551 3.2753 13.7994 3.90317 12 6.00019Z"
-                      stroke="#333"
-                      strokeWidth="2"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    ></path>
-                  </g>
-                </svg>
-                Wish List
               </Link>
             </li>
           </>
