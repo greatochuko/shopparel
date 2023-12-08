6db7c474217f3d4354b6848d55ce290d5272d0f1
Great Ogheneochuko <greatochuko123@gmail.com>
2023-12-08 10:27:30 +0100

    create fade in animation for modal container

--- client/src/components/Modal.tsx
+++ client/src/components/Modal.tsx
@@ -2,7 +2,7 @@ export default function Modal({ closeModal }: { closeModal: () => void }) {
   return (
     <div
       onClick={closeModal}
-      className="fixed z-50 bg-black/20 top-0 left-0 w-screen h-screen flex-center"
+      className="fixed z-50 bg-black/20 top-0 left-0 w-screen h-screen flex-center animate-fade-in"
     >
       <div className="w-[90%] max-w-xl bg-white aspect-video rounded-lg animate-zoom-in"></div>
     </div>
