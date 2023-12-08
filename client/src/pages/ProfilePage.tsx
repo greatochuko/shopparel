e28491643073cf4ef0178ba2e829cbb0ad788431
Great Ogheneochuko <greatochuko123@gmail.com>
2023-12-08 01:56:48 +0100

    add focus styles to footer links

--- client/src/pages/ProfilePage.tsx
+++ client/src/pages/ProfilePage.tsx
@@ -1,8 +1,17 @@
+import { Link } from "react-router-dom";
+
 export default function ProfilePage() {
   return (
     <main className="mt-[78px] pt-1 flex flex-col gap-4  max-w-7xl w-[90%] mx-auto mb-2 text-zinc-700">
       <p>
-        Home &gt; My Account &gt;{" "}
+        <Link
+          to={"/"}
+          className="hover:underline focus-visible:ring ring-blue-400 rounded-md p-1"
+        >
+          {" "}
+          Home
+        </Link>{" "}
+        &gt; My Account &gt;{" "}
         <span className="font-semibold">Personal Info</span>
       </p>
     </main>
