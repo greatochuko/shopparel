deda20d90805f63a349bc92220c09c2d48b29733
Great Ogheneochuko <greatochuko123@gmail.com>
2023-12-12 14:00:06 +0100

    create product and review models

---
+++ server/models/Product.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  imgUrl: { type: String, required: true },
+  brand: { type: String, required: true },
+  price: { type: Number, required: true },
+  images: { type: [String], required: true },
+  rating: { type: Number, default: 5 },
+  reviews: {
+    type: [mongoose.SchemaTypes.ObjectId],
+    ref: "review",
+    default: [],
+  },
+  sizes: { type: [String], required: true },
+  categories: { type: [String], required: true },
+  colors: { type: [String], required: true },
+});
+
+export const Product = mongoose.model("product", productSchema);
---
+++ server/models/Review.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const reviewSchema = new mongoose.Schema(
+  {
+    user: { type: mongoose.SchemaTypes.ObjectId, required: true },
+    rating: { type: Number, required: true },
+    review: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+export const Review = mongoose.model("review", reviewSchema);
