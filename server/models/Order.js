1fbf4557da794ce89336e2ecc7e4e6962e44223f
Great Ogheneochuko <greatochuko123@gmail.com>
2023-12-13 06:05:09 +0100

    add userId to order schema

--- server/models/Order.js
+++ server/models/Order.js
@@ -2,9 +2,14 @@ import mongoose from "mongoose";
 
 const orderSchema = new mongoose.Schema(
   {
+    userId: {
+      type: mongoose.SchemaTypes.ObjectId,
+      required: true,
+      ref: "user",
+    },
     deliveryDate: { type: Date, required: true },
     status: { type: String, default: "active" },
-    status: { type: String, required: true },
+    paymentMethod: { type: String, required: true },
     products: {
       type: [mongoose.SchemaTypes.ObjectId],
       required: true,
