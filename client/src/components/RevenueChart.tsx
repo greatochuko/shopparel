87f36ed6932553942cc758aa3be6fbe5548b2e4e
Great Ogheneochuko <greatochuko123@gmail.com>
2024-02-05 10:03:20 +0100

    move revenue chart bar into separate component and redesign chart bar tooltip

--- client/src/components/RevenueChart.tsx
+++ client/src/components/RevenueChart.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { AdminOrderType } from "./AdminOrder";
+import RevenueChartBar from "./RevenueChartBar";
 
 const data = [
   {
@@ -87,10 +88,10 @@ export default function RevenueChart({
   });
 
   return (
-    <div className="aspect-video flex flex-col gap-4 bg-white p-4 shadow rounded-md">
-      <header className="flex justify-between items-center">
+    <div className="flex flex-col gap-4 p-4 bg-white rounded-md shadow aspect-video">
+      <header className="flex items-center justify-between">
         <h2 className="text-lg font-semibold">Sales Revenue</h2>
-        <div className="flex gap-2 p-2 rounded-full bg-gray-100 text-sm">
+        <div className="flex gap-2 p-2 text-sm bg-gray-100 rounded-full">
           <button
             onClick={() => setYear(2022)}
             className={`rounded-full duration-300 font-semibold flex-center h-6 w-12 ${
@@ -125,14 +126,14 @@ export default function RevenueChart({
       </header>
       <div className="flex h-[85%] justify-between relative">
         <div className="flex absolute pl-[7%] top-0 w-full h-full gap-4 flex-col">
-          <div className="border-t border-zinc-200 border-dashed flex-1"></div>
-          <div className="border-t border-zinc-200 border-dashed flex-1"></div>
-          <div className="border-t border-zinc-200 border-dashed flex-1"></div>
-          <div className="border-t border-zinc-200 border-dashed flex-1"></div>
-          <div className="border-t border-zinc-200 border-dashed flex-1"></div>
-          <div className="border-t border-zinc-200 border-dashed flex-1"></div>
-          <div className="border-t border-zinc-200 border-dashed flex-1"></div>
-          <div className="border-t border-zinc-200 border-dashed flex-1"></div>
+          <div className="flex-1 border-t border-dashed border-zinc-200"></div>
+          <div className="flex-1 border-t border-dashed border-zinc-200"></div>
+          <div className="flex-1 border-t border-dashed border-zinc-200"></div>
+          <div className="flex-1 border-t border-dashed border-zinc-200"></div>
+          <div className="flex-1 border-t border-dashed border-zinc-200"></div>
+          <div className="flex-1 border-t border-dashed border-zinc-200"></div>
+          <div className="flex-1 border-t border-dashed border-zinc-200"></div>
+          <div className="flex-1 border-t border-dashed border-zinc-200"></div>
         </div>
         <div className="flex flex-col gap-4 items-center z-[2]">
           <p className="flex-1 -translate-y-[50%] flex-center text-sm sm:text-base">
@@ -160,28 +161,13 @@ export default function RevenueChart({
             $0
           </p>
         </div>
-        {data.map((month) => (
-          <div
+        {data.map((month, i) => (
+          <RevenueChartBar
             key={month.month}
-            className="flex flex-col justify-end items-center w-[5%] z-[2]"
-          >
-            <div
-              style={{ height: ((month.amount / 700) * 100).toFixed() + "%" }}
-              tabIndex={month.amount ? 0 : undefined}
-              className="w-full relative rounded-t-full group active:bg-accent-blue-300 focus-visible:bg-accent-blue-300 hover:bg-accent-blue-200 duration-500 cursor-pointer bg-accent-blue-100"
-            >
-              <div className="flex-center flex-col opacity-0 duration-300 group-hover:opacity-100 absolute left-[50%] -translate-y-[100%] -translate-x-[50%]">
-                <div className="bg-zinc-700 font-sm top-0 px-3 py-1 rounded-md text-white w-fit">
-                  ${month.amount}
-                </div>
-                <div className="bg-transparent border-[12px] border-transparent border-t-zinc-700 -mt-1 w-0 h-0"></div>
-              </div>
-            </div>
-            <p className="sm:block hidden h-[8.5%] xl:h-[9.9%] sm:flex-center">
-              {month.month}
-            </p>
-            <p className="sm:hidden text-sm">{month.month.slice(0, 1)}</p>
-          </div>
+            month={month}
+            index={i}
+            data={data}
+          />
         ))}
       </div>
     </div>
