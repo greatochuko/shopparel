c3f18c3c0dc06c0027090dbfb61679c197052394
Great Ogheneochuko <greatochuko123@gmail.com>
2024-02-14 13:09:12 +0100

    enhance the appearance of the full screen loader

--- client/src/components/FullScreenLoader.tsx
+++ client/src/components/FullScreenLoader.tsx
@@ -1,9 +1,33 @@
+import { useState } from "react";
+
 export default function FullScreenLoader() {
+  const [showBlur, setShowBlur] = useState(true);
+
+  console.log(showBlur);
+
   return (
-    <div className="flex-center h-screen flex-col gap-4">
-      <h1 className="text-3xl font-bold text-zinc-800">Shopparel</h1>
-      <div className="w-32 bg-zinc-200 h-1 relative rounded-full overflow-hidden">
-        <div className="animate-loader absolute w-full h-full bg-zinc-700"></div>
+    <div className="">
+      <img
+        src="/full-screen-bg-blur.jpg"
+        alt="clothes store"
+        className={`fixed object-cover w-screen h-screen -z-10 duration-300 `}
+      />
+      <img
+        onLoad={() => {
+          setShowBlur(false);
+        }}
+        src="/full-screen-bg.jpg"
+        alt="clothes store"
+        className={`fixed object-cover w-screen h-screen -z-10 duration-[10000] animate-fade-in-duration-1000 ${
+          showBlur ? "hidden" : "block"
+        }`}
+      />
+      <div className="flex-col w-screen h-screen gap-4 bg-black/50 flex-center">
+        <img src="/favicon.png" alt="" width={50} height={50} />
+        <h1 className="text-3xl font-bold text-white">Shopparel</h1>
+        <div className="relative w-32 h-1 overflow-hidden rounded-full bg-black/50">
+          <div className="absolute w-full h-full bg-white animate-loader"></div>
+        </div>
       </div>
     </div>
   );
