5cd95e2a1733168b62042108d70ebd7656055e44
Great Ogheneochuko <greatochuko123@gmail.com>
2024-03-04 12:14:55 +0100

    remove authenticate middleware from get store route and get store products route

--- server/routes/storeRoutes.js
+++ server/routes/storeRoutes.js
@@ -13,9 +13,9 @@ const storeRouter = Router();
 
 storeRouter.get("/stores", getStores);
 storeRouter.post("/store", authenticateUser, createStore);
-storeRouter.get("/store/:storeId", authenticateUser, getStore);
+storeRouter.get("/store/:storeId", getStore);
 storeRouter.patch("/store/:storeId", authenticateUser, updateStore);
-storeRouter.get("/store/:storeId/products", authenticateUser, getStoreProducts);
+storeRouter.get("/store/:storeId/products", getStoreProducts);
 storeRouter.get("/store/:storeId/orders", authenticateUser, getStoreOrders);
 
 export default storeRouter;
